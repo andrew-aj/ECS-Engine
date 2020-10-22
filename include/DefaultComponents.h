c8553818c0a3fb1cb7b08a78c7980e1566be2c9b
cheezy1231 <andrew.knee.j@gmail.com>
2020-10-21 22:27:05 -0400

    Created documentation for functions.

--- include/DefaultComponents.h
+++ include/DefaultComponents.h
@@ -8,57 +8,366 @@
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/quaternion.hpp>
 #include <glm/glm.hpp>
+#include <array>
 
 namespace ClockworkEngine {
+    /**
+     * A Transform class for which the 3D data is taken from to determine characteristics of model.
+     */
     struct Transform {
-        glm::vec3 position;
-        glm::quat quatRotation;
-        glm::vec3 scale;
-
-        glm::quat degreeToQuat(glm::vec3 angle) {
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in degrees.
+         * @return Quaternion created from the 3 euler angles. Optional return. Automatically
+         *      updates the quaternion stored in the struct.
+         */
+        glm::quat degreeToQuat(const glm::vec3 &angle) {
             quatRotation = glm::quat(glm::radians(angle));
             return quatRotation;
         }
 
-        static glm::quat degreeToQuat(glm::quat quatRotation, glm::vec3 angle) {
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param quatRotation Quaternion for which the rotation is stored.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in degress.
+         * @return Quaternion created from the 3 euler angles.
+         */
+        static void degreeToQuat(glm::quat &quatRotation, const glm::vec3 &angle) {
             quatRotation = glm::quat(glm::radians(angle));
-            return quatRotation;
         }
 
-        glm::quat radiansToQuat(glm::vec3 angle) {
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in radians.
+         * @return Quaternion created from the 3 euler angles. Optional return. Automatically
+         *      updates the quaternion stored in the struct.
+         */
+        glm::quat radiansToQuat(const glm::vec3 &angle) {
             quatRotation = glm::quat(angle);
             return quatRotation;
         }
 
-        static glm::quat radiansToQuat(glm::quat quatRotation, glm::vec3 angle) {
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param quatRotation Quaternion for which the rotation is stored.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in radians.
+         * @return Quaternion created from the 3 euler angles.
+         */
+        static void radiansToQuat(glm::quat &quatRotation, const glm::vec3 &angle) {
             quatRotation = glm::quat(angle);
-            return quatRotation;
         }
 
-        glm::quat axisToQuat(float rotationRads, glm::vec3 rotationAxis) {
+        /**
+         * Creates a quaternion from a rotation axis and the orientation around that axis.
+         * @param rotationRads Float of the rotation around the axis in radians.
+         * @param rotationAxis Vec3 of the rotation axis in form {x, y, z}.
+         * @return Quaternion created from the axes and angle. Optional return. Automatically
+         *      updates the quaternion stored in the struct.
+         */
+        glm::quat axisToQuat(const float &rotationRads, const glm::vec3 &rotationAxis) {
             quatRotation = glm::angleAxis(rotationRads, rotationAxis);
             return quatRotation;
         }
 
-        static glm::quat axisToQuat(glm::quat quatRotation, float rotationRads, glm::vec3 rotationAxis) {
+        /**
+         * Creates a quaternion from a rotation axis and the orientation around that axis.
+         * @param quatRotation Quaternion for which the rotation is stored.
+         * @param rotationRads Float of the rotation around the axis in radians.
+         * @param rotationAxis Vec3 of the rotation axis in form {x, y, z}.
+         * @return Quaternion created from the axes and angle.
+         */
+        static void axisToQuat(glm::quat &quatRotation, const float &rotationRads, const glm::vec3 &rotationAxis) {
             quatRotation = glm::angleAxis(rotationRads, rotationAxis);
-            return quatRotation;
         }
 
-        glm::mat4 toMat4(){
+        /**
+         * Converts quaternion stored in struct into a 4x4 matrix.
+         * @return Mat4 constructed from quaternion.
+         */
+        glm::mat4 toMat4() const {
             return glm::toMat4(quatRotation);
         }
 
-        static glm::mat4 toMat4(glm::quat quaternion){
+        /**
+         * Converts quaternion into a 4x4 matrix.
+         * @param quaternion Quaternion to convert to Mat4.
+         * @return Mat4 constructed from quaternion.
+         */
+        static glm::mat4 toMat4(const glm::quat &quaternion) {
             return glm::toMat4(quaternion);
         }
 
-        glm::vec3 toEuler(){
+        /**
+         * Converts quaternion stored in struct into a Vec3 or euler angles.
+         * @return Vec3 of euler angles in form {x, y, z}.
+         */
+        glm::vec3 toEuler() const {
             return glm::eulerAngles(quatRotation);
         }
 
-        static glm::vec3 toEuler(glm::quat quatRotation){
+        /**
+         * Converts quaternion into a Vec3 or euler angles.
+         * @param quatRotation Quaternion to convert to Vec3.
+         * @return Vec3 of euler angles in form {x, y, z}.
+         */
+        static glm::vec3 toEuler(const glm::quat &quatRotation) {
             return glm::eulerAngles(quatRotation);
+        }
+
+        const glm::vec3 &getPosition() const {
+            return position;
+        }
+
+        const glm::quat &getQuatRotation() const {
+            return quatRotation;
+        }
+
+        const glm::vec3 &getScale() const {
+            return scale;
+        }
+
+        void setPosition(const glm::vec3 &position) {
+            Transform::position = position;
+        }
+
+        void setQuatRotation(const glm::quat &quatRotation) {
+            Transform::quatRotation = quatRotation;
+        }
+
+        void setScale(const glm::vec3 &scale) {
+            Transform::scale = scale;
+        }
+
+    private:
+        glm::vec3 position;
+        glm::quat quatRotation;
+        glm::vec3 scale;
+    };
+
+
+    /**
+     * Stores all relevant physics information of 3D rigidbody.
+     * Used in conjunction with transform for which the variables in here are set in the transform.
+     */
+    struct RigidBody {
+        glm::vec3 velocity;
+        glm::vec3 position;
+        glm::quat rotation;
+        glm::vec3 angularVelocity;
+
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in degrees.
+         * @return Quaternion created from the 3 euler angles. Optional return. Automatically
+         *      updates the quaternion stored in the struct.
+         */
+        glm::quat degreeToQuat(const glm::vec3 &angle) {
+            rotation = glm::quat(glm::radians(angle));
+            return rotation;
+        }
+
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param rotation Quaternion for which the rotation is stored.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in degress.
+         * @return Quaternion created from the 3 euler angles.
+         */
+        static void degreeToQuat(glm::quat &rotation, const glm::vec3 &angle) {
+            rotation = glm::quat(glm::radians(angle));
+        }
+
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in radians.
+         * @return Quaternion created from the 3 euler angles. Optional return. Automatically
+         *      updates the quaternion stored in the struct.
+         */
+        glm::quat radiansToQuat(const glm::vec3 &angle) {
+            rotation = glm::quat(angle);
+            return rotation;
+        }
+
+        /**
+         * Creates a quaternion from 3 euler angles.
+         * @param rotation Quaternion for which the rotation is stored.
+         * @param angle Vec3 containing the euler angles in form {x, y, z} in radians.
+         * @return Quaternion created from the 3 euler angles.
+         */
+        static void radiansToQuat(glm::quat &rotation, const glm::vec3 &angle) {
+            rotation = glm::quat(angle);
+        }
+
+        /**
+         * Creates a quaternion from a rotation axis and the orientation around that axis.
+         * @param rotationRads Float of the rotation around the axis in radians.
+         * @param rotationAxis Vec3 of the rotation axis in form {x, y, z}.
+         * @return Quaternion created from the axes and angle. Optional return. Automatically
+         *      updates the quaternion stored in the struct.
+         */
+        glm::quat axisToQuat(const float &rotationRads, const glm::vec3 &rotationAxis) {
+            rotation = glm::angleAxis(rotationRads, rotationAxis);
+            return rotation;
+        }
+
+        /**
+         * Creates a quaternion from a rotation axis and the orientation around that axis.
+         * @param rotation Quaternion for which the rotation is stored.
+         * @param rotationRads Float of the rotation around the axis in radians.
+         * @param rotationAxis Vec3 of the rotation axis in form {x, y, z}.
+         * @return Quaternion created from the axes and angle.
+         */
+        static void axisToQuat(glm::quat &rotation, const float &rotationRads, const glm::vec3 &rotationAxis) {
+            rotation = glm::angleAxis(rotationRads, rotationAxis);
+        }
+
+        /**
+         * Converts quaternion stored in struct into a 4x4 matrix.
+         * @return Mat4 constructed from quaternion.
+         */
+        glm::mat4 toMat4() const {
+            return glm::toMat4(rotation);
+        }
+
+        /**
+         * Converts quaternion into a 4x4 matrix.
+         * @param quaternion Quaternion to convert to Mat4.
+         * @return Mat4 constructed from quaternion.
+         */
+        static glm::mat4 toMat4(const glm::quat &quaternion) {
+            return glm::toMat4(quaternion);
+        }
+
+        /**
+         * Converts quaternion stored in struct into a Vec3 or euler angles.
+         * @return Vec3 of euler angles in form {x, y, z}.
+         */
+        glm::vec3 toEuler() const {
+            return glm::eulerAngles(rotation);
+        }
+
+        /**
+         * Converts quaternion into a Vec3 or euler angles.
+         * @param rotation Quaternion to convert to Vec3.
+         * @return Vec3 of euler angles in form {x, y, z}.
+         */
+        static glm::vec3 toEuler(const glm::quat &rotation) {
+            return glm::eulerAngles(rotation);
+        }
+
+        const glm::vec3 &getVelocity() const {
+            return velocity;
+        }
+
+        const glm::vec3 &getPosition() const {
+            return position;
+        }
+
+        const glm::quat &getRotation() const {
+            return rotation;
+        }
+
+        const glm::vec3 &getAngularVelocity() const {
+            return angularVelocity;
+        }
+
+        void setVelocity(const glm::vec3 &velocity) {
+            RigidBody::velocity = velocity;
+        }
+
+        void setPosition(const glm::vec3 &position) {
+            RigidBody::position = position;
+        }
+
+        void setRotation(const glm::quat &rotation) {
+            RigidBody::rotation = rotation;
+        }
+
+        void setAngularVelocity(const glm::vec3 &angularVelocity) {
+            RigidBody::angularVelocity = angularVelocity;
+        }
+    };
+
+    /**
+     * Contains the mesh data.
+     * Stores the Vec3 of vertices, normals, and the pairing of vertices to create polygons.
+     */
+    struct Mesh {
+        int verticesSize = 0, normalsSize = 0, orderOfVerticesSize = 0;
+        glm::vec3 *vertices;
+        glm::vec3 *normals;
+        glm::vec3 *orderOfVertices;
+
+        /**
+         * Sets the vertices stored in the object.
+         * @param vertices Vec3 array of the vertex vectors.
+         * @param length Integer of the length of the array.
+         */
+        void setVertices(glm::vec3 vertices[], int length) {
+            delete[] vertices;
+            Mesh::vertices = vertices;
+            verticesSize = length;
+        }
+
+        /**
+         * Sets the normals stored in the object.
+         * @param normals Vec3 array of the normal vectors.
+         * @param length Integer of the length of the array.
+         */
+        void setNormals(glm::vec3 normals[], int length) {
+            delete[] Mesh::normals;
+            Mesh::normals = normals;
+            normalsSize = length;
+        }
+
+        /**
+         * Sets the order of the vertices stored in the object.
+         * @param orderOfVertices Vec3 array of the order of the vertices.
+         *      Vec3 consists of indices of the vertices from the original array.
+         * @param length Integer of the length of the array.
+         */
+        void setOrderOfVertices(glm::vec3 orderOfVertices[], int length) {
+            delete[] Mesh::orderOfVertices;
+            Mesh::orderOfVertices = orderOfVertices;
+            orderOfVerticesSize = length;
+        }
+
+        /**
+         * Returns the vertices.
+         * @param vertices Vec3 pointer to store the value of the vertices.
+         * @param size Integer reference to get size of the array.
+         */
+        void getVertices(glm::vec3 *vertices, int &size) {
+            size = verticesSize;
+            vertices = Mesh::vertices;
+        }
+
+        /**
+         * Returns the normals.
+         * @param normals Vec3 pointer to store the value of the normals.
+         * @param size Integer reference to get size of the array.
+         */
+        void getNormals(glm::vec3 *normals, int &size) {
+            size = normalsSize;
+            normals = Mesh::normals;
+        }
+
+        /**
+         * Returns the order of the vertices.
+         * @param orderOfVertices Vec3 pointer to store the value of the order.
+         * @param size Integer reference to get size of the array.
+         */
+        void getOrder(glm::vec3 *orderOfVertices, int &size) {
+            size = orderOfVerticesSize;
+            orderOfVertices = Mesh::orderOfVertices;
+        }
+
+        /**
+         * Deletes the pointers.
+         */
+        ~Mesh() {
+            delete[] vertices;
+            delete[] normals;
+            delete[] orderOfVertices;
         }
     };
 }
--- include/Manager.h
+++ include/Manager.h
@@ -17,14 +17,25 @@
 
 namespace ClockworkEngine {
 
+    /**
+     * Manager class that stores and keeps track of the entities and components.
+     */
     class Manager {
     public:
+        /**
+         * Creates the containers.
+         */
         Manager() {
             componentToID = std::map<std::type_index, ComponentID>();
             freeIDs = std::queue<EntityID>();
             componentMap = std::map<ComponentID, std::map<EntityID, std::any>>();
         }
 
+        /**
+         * Creates and entity and increments the next available ID.
+         * If there is a free ID then it takes that to conserve memory and reduce the max ID value used.
+         * @return EntityID that stores the integer ID value for the entity created.
+         */
         EntityID createEntity() {
             EntityID temp;
             if (!freeIDs.empty()) {
@@ -40,10 +51,15 @@ namespace ClockworkEngine {
 
         }
 
+        /**
+         * Frees and entity and removes all of its components. Adds it to the list of
+         * free entity IDs.
+         * @param entity EntityID to use.
+         */
         void freeEntity(EntityID entity) {
-            for(auto a : componentMap){
+            for (auto a : componentMap) {
                 auto it = a.second.find(entity);
-                if(it != a.second.end()){
+                if (it != a.second.end()) {
                     it->second.reset();
                     a.second.erase(entity);
                 }
@@ -51,11 +67,17 @@ namespace ClockworkEngine {
             freeIDs.push(entity);
         }
 
+        /**
+         * Creates a component and adds it to the entity ID.
+         * @tparam T Component type.
+         * @param entity EntityID to assign component to.
+         * @return Weak_ptr of type T to eliminate conflicting ptr problems.
+         */
         template<typename T>
-        std::weak_ptr<T> createComponent(EntityID entity){
+        std::weak_ptr<T> createComponent(EntityID entity) {
             assert(std::is_aggregate_v<T>);
             auto it = componentToID.find(typeid(T));
-            if(it == componentToID.end()){
+            if (it == componentToID.end()) {
                 componentToID[typeid(T)] = nextComponentID++;
             }
             std::shared_ptr<T> createdPtr = std::make_shared<T>();
@@ -63,17 +85,37 @@ namespace ClockworkEngine {
             return createdPtr;
         }
 
-        template <typename T>
+        /**
+         * Removes the component from the associated entity.
+         * @tparam T Component type.
+         * @param entityId EntityID to assign component to.
+         */
+        template<typename T>
         void removeComponent(EntityID entityId) {
             componentMap[componentToID[typeid(T)]][entityId].reset();
             componentMap[componentToID[typeid(T)]].erase(entityId);
         }
 
+        /**
+         * Gets the component of type T for the entity.
+         * @tparam T Component type.
+         * @param entity EntityID to assign component to.
+         * @return Weak_ptr of type T containing the component data.
+         */
         template<typename T>
-        std::shared_ptr<T> getComponent(EntityID entity) {
-            return componentMap[componentToID[typeid(T)]][entity];
+        std::weak_ptr<T> getComponent(EntityID entity) {
+            if (componentMap[componentToID[typeid(T)]].find(entity) != componentMap[componentToID[typeid(T)]].end()) {
+                return componentMap[componentToID[typeid(T)]][entity];
+            }else{
+                return nullptr;
+            }
         }
 
+        /**
+         * Gets the entities with a component of type T.
+         * @tparam T Component type.
+         * @return std::pair of index 0 equal to iterator.begin() and index 1 equal to iterator.end().
+         */
         template<typename T>
         std::pair<std::map<EntityID, std::any>::iterator, std::map<EntityID, std::any>::iterator> getEntities() {
             return {componentMap[componentToID[typeid(T)]].begin(), componentMap[componentToID[typeid(T)]].end()};

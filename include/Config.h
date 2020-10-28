2b3a48c136ca77410afa5cedeee669424ccd38e9
cheezy1231 <andrew.knee.j@gmail.com>
2020-10-28 19:18:20 -0400

    Added pre-defining the file path.

--- include/Config.h
+++ include/Config.h
@@ -19,14 +19,21 @@ namespace ClockworkEngine {
     public:
         Config(std::vector<std::string> valuesForConfig);
 
+        Config(std::vector<std::string> valuesForConfig, const std::string &filePath);
+
         ~Config();
 
         bool loadConfig(const std::string &filePath);
 
         bool writeConfig(const std::string &filePath);
+
+        bool loadConfig();
+
+        bool writeConfig();
     private:
         std::unordered_map<std::string, std::string> configValues;
         static std::vector<std::string> configFiles;
+        const std::string &FilePath;
         std::mutex m;
     };
 }
--- src/Config.cpp
+++ src/Config.cpp
@@ -7,7 +7,13 @@
 namespace ClockworkEngine {
     std::vector<std::string> Config::configFiles;
 
-    Config::Config(std::vector<std::string> valuesForConfig) {
+    Config::Config(std::vector<std::string> valuesForConfig) : FilePath(nullptr){
+        for (auto &i : valuesForConfig) {
+            configValues.insert({i, nullptr});
+        }
+    }
+
+    Config::Config(std::vector<std::string> valuesForConfig, const std::string &filePath) : FilePath(filePath){
         for (auto &i : valuesForConfig) {
             configValues.insert({i, nullptr});
         }
@@ -68,9 +74,9 @@ namespace ClockworkEngine {
         std::stringstream ss;
 
         configFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        try{
-            if(configFile.is_open()){
-                for(auto& a : configValues){
+        try {
+            if (configFile.is_open()) {
+                for (auto &a : configValues) {
                     ss << a.first << "=" << a.second << std::endl;
                 }
                 configFile << ss.str();
@@ -86,6 +92,81 @@ namespace ClockworkEngine {
         }
         catch (std::ifstream::failure &e) {
             std::cout << "ERROR::CONFIG::" << filePath << "::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+            if (found) {
+                m.unlock();
+            }
+            return false;
+        }
+    }
+
+    bool Config::loadConfig() {
+        bool found = false;
+        if (std::find(configFiles.begin(), configFiles.end(), FilePath) == configFiles.end()) {
+            configFiles.push_back(FilePath);
+        } else {
+            found = true;
+            m.lock();
+        }
+        std::ifstream configFile;
+
+        //ensure ifstream objects can throw exceptions:
+        configFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try {
+            if (configFile.is_open()) {
+                std::string line;
+                while (std::getline(configFile, line)) {
+                    int location = line.find('=');
+                    configValues[line.substr(0, location)] = line.substr(location + 1);
+                }
+                if (found) {
+                    m.unlock();
+                }
+                return true;
+            }
+            if (found) {
+                m.unlock();
+            }
+            return false;
+        }
+        catch (std::ifstream::failure &e) {
+            std::cout << "ERROR::CONFIG::" << FilePath << "::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+            if (found) {
+                m.unlock();
+            }
+            return false;
+        }
+    }
+
+    bool Config::writeConfig() {
+        bool found = false;
+        if (std::find(configFiles.begin(), configFiles.end(), FilePath) == configFiles.end()) {
+            configFiles.push_back(FilePath);
+        } else {
+            found = true;
+            m.lock();
+        }
+        std::ofstream configFile;
+        std::stringstream ss;
+
+        configFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try {
+            if (configFile.is_open()) {
+                for (auto &a : configValues) {
+                    ss << a.first << "=" << a.second << std::endl;
+                }
+                configFile << ss.str();
+                if (found) {
+                    m.unlock();
+                }
+                return true;
+            }
+            if (found) {
+                m.unlock();
+            }
+            return false;
+        }
+        catch (std::ifstream::failure &e) {
+            std::cout << "ERROR::CONFIG::" << FilePath << "::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
             if (found) {
                 m.unlock();
             }

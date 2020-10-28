182561032a48b3fc92a62b60e188d002dcc0f67e
cheezy1231 <andrew.knee.j@gmail.com>
2020-10-28 11:52:21 -0400

    Added config reading.

--- include/Config.h
+++ include/Config.h
@@ -8,12 +8,18 @@
 #include "Defines.h"
 #include <unordered_map>
 #include <vector>
+#include <fstream>
+#include <iostream>
 
 namespace ClockworkEngine {
     class Config {
     public:
         Config(std::vector<std::string> valuesForConfig);
+
         ~Config();
+
+        void loadConfig(const std::string &filePath);
+
     private:
         std::unordered_map<std::string, std::string> configValues;
     };
--- src/Config.cpp
+++ src/Config.cpp
@@ -14,4 +14,25 @@ namespace ClockworkEngine{
     Config::~Config() {
         configValues.erase(configValues.begin(), configValues.end());
     }
+
+    //config stored as config_string=data
+    void Config::loadConfig(const std::string &filePath){
+        std::string vertexCode;
+        std::ifstream configFile;
+
+        //ensure ifstream objects can throw exceptions:
+        configFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try{
+            if(configFile.is_open()){
+                std::string line;
+                while(std::getline(configFile, line)){
+                    int location = line.find('=');
+                    configValues[line.substr(0, location)] = line.substr(location+1);
+                }
+            }
+        }
+        catch(std::ifstream::failure& e){
+            std::cout << "ERROR::CONFIG::" << filePath << "::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+        }
+    }
 }
